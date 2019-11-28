ba24cd3fd1a8134ba05b0f118e49cd35403500ae
lijianjie <604764332@qq.com>
2019-11-28 23:38:40 +0800

    整合发送邮件

--- demo/src/main/java/com/test/controller/DemoController.java
+++ demo/src/main/java/com/test/controller/DemoController.java
@@ -1,19 +1,37 @@
 package com.test.controller;
 
+import com.test.mail.SendMailTest;
+import com.test.utils.ResultResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "测试swagger")
 @RequestMapping("/demo")
+@Slf4j
 public class DemoController {
 
-    @RequestMapping("/")
+    @Autowired
+    private SendMailTest sendMailTest;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     @ApiOperation("返回测试数据")
     public String helloDemo() {
         return "Hello demo";
+    }
+
+    @RequestMapping(value = "/testSendMail", method = RequestMethod.GET)
+    @ApiOperation("测试返回邮件")
+    public ResultResponse testSendMail() {
+        log.info("测试发送邮件开始~~~~~~~~~~~~~~~");
+        sendMailTest.sendMail();
+        log.info("测试发送邮件结束~~~~~~~~~~~~~~~");
+        return ResultResponse.success();
     }
 }
--- demo/src/main/java/com/test/mail/SendMailTest.java
+++ demo/src/main/java/com/test/mail/SendMailTest.java
@@ -1,9 +1,15 @@
+
 package com.test.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Component
 public class SendMailTest {
@@ -12,11 +18,29 @@ public class SendMailTest {
     private JavaMailSender javaMailSender;
 
     public void sendMail() {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("604764332@qq.com");
-        simpleMailMessage.setTo("");
-        simpleMailMessage.setSubject("标题：");
-        simpleMailMessage.setText("内容");
-        javaMailSender.send(simpleMailMessage);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("604764332@qq.com");
+            helper.setTo("347731846@qq.com");
+            helper.setSubject("标题");
+            StringBuffer sb = new StringBuffer();
+            sb.append("<h1>大标题-h1</h1>")
+                    .append("<p style='color:#F00'>红色字</p>")
+                    .append("<p style='text-align:right'>右对齐</p>");
+            helper.setText(sb.toString(), true);
+            FileSystemResource fileSystemResource=new FileSystemResource(new File("D:\\BugReport.txt"));
+            helper.addAttachment("123", fileSystemResource); // 文件名字 文件（附件）
+            javaMailSender.send(message);
+        } catch (Exception e) {
+
+        }
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        simpleMailMessage.setFrom("604764332@qq.com");
+//        simpleMailMessage.setTo("1426387157@qq.com");
+//        simpleMailMessage.setSubject("标题：");
+//        simpleMailMessage.setText("内容");
+//        simpleMailMessage.
+//        javaMailSender.send(simpleMailMessage);
     }
 }
